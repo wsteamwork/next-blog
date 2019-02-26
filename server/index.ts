931466ecd0b97cd19b3f09d08db20ea9b86af5f0
HarikiRito <nxh0809@gmail.com>
2019-02-26 14:08:35 +0700

    #21 Port with dotenv

--- server/index.ts
+++ server/index.ts
@@ -4,6 +4,7 @@ import {Request, Response} from 'express';
 import * as compression from 'compression';
 import * as helmet from 'helmet';
 
+require('dotenv').config();
 const port   = parseInt(process.env.PORT, 10) || 3000;
 const dev    = process.env.NODE_ENV !== 'production';
 const app    = next({dev});
